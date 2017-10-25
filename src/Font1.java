1cc787eaacb2d0365c037e7fa1c52c7a938eea5e
CloudsRosy <lzn1024617@foxmail.com>
2017-10-25 17:02:44 +0800

    优化保存和另存为

--- src/Font1.java
+++ src/Font1.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Font1 extends JDialog implements ItemListener{
+class Font1 extends JDialog implements ItemListener {
     /**
      *
      */
@@ -13,47 +13,49 @@ public class Font1 extends JDialog implements ItemListener{
     JComboBox comboBox1 = new JComboBox();
     JComboBox comboBox2 = new JComboBox();
     JComboBox comboBox3 = new JComboBox();
-    JLabel lab1=new JLabel("字体：");
-    JLabel lab2=new JLabel("字形：");
-    JLabel lab3=new JLabel("字号：");
-    String name=new String("宋体");
-    Font f1=new Font("隶书",Font.PLAIN,15);
-    int style=1;
-    int size=12;
-    String []array2=new String[]{"常       规","倾        斜","加       粗"};
-    String []array3=new String[]{"14","15","15","16","17","18"};
-    GraphicsEnvironment ge=GraphicsEnvironment.getLocalGraphicsEnvironment();
-    String [] fontName=ge.getAvailableFontFamilyNames();
-    JButton b1=new JButton("确定");
-    JButton b2=new JButton("颜色");
-    JTextArea a1=new JTextArea();
-    void set(JTextArea n){
-        a1=n;
+    JLabel lab1 = new JLabel("字体：");
+    JLabel lab2 = new JLabel("字形：");
+    JLabel lab3 = new JLabel("字号：");
+    String name = new String("宋体");
+    Font f1 = new Font("隶书", Font.PLAIN, 15);
+    int style = 1;
+    int size = 12;
+    String[] array2 = new String[]{"常       规", "倾        斜", "加       粗"};
+    String[] array3 = new String[]{"14", "15", "15", "16", "17", "18"};
+    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    String[] fontName = ge.getAvailableFontFamilyNames();
+    JButton b1 = new JButton("确定");
+    JButton b2 = new JButton("颜色");
+    JTextArea a1 = new JTextArea();
+
+    void set(JTextArea n) {
+        a1 = n;
     }
-    public Font1(){
+
+    public Font1() {
         setTitle("字体");
-        setSize(500,600);
+        setSize(500, 600);
         setLayout(new FlowLayout());
         //panel1.setLocation(100, 200);
         lab1.setFont(f1);
         lab2.setFont(f1);
         comboBox1.setModel(new DefaultComboBoxModel(fontName));
         comboBox1.setFont(f1);
-        for(int i=1;i<fontName.length;i++){
+        for (int i = 1; i < fontName.length; i++) {
             //comboBox1.setSelectedIndex(i);
             comboBox1.setSelectedItem(fontName);
             //comboBox1.addItem(fontName);
         }
         comboBox2.setModel(new DefaultComboBoxModel(array2));
         comboBox2.setFont(f1);
-        for(int i=1;i<array2.length;i++){
+        for (int i = 1; i < array2.length; i++) {
             //comboBox2.setSelectedIndex(i);
             comboBox2.setSelectedItem(array2);
             //comboBox2.addItem(array2);
         }
         comboBox3.setModel(new DefaultComboBoxModel(array3));
         comboBox3.setFont(f1);
-        for(int i=1;i<array3.length;i++){
+        for (int i = 1; i < array3.length; i++) {
             //comboBox2.setSelectedIndex(i);
             comboBox2.setSelectedItem(array3);
             //comboBox3.addItem(array3);
@@ -76,40 +78,42 @@ public class Font1 extends JDialog implements ItemListener{
         add(panel2);
         add(panel3);
     }
+
     public void itemStateChanged(ItemEvent e) {
-        if(e.getSource()==comboBox1){
-            name=comboBox1.getSelectedItem().toString();
+        if (e.getSource() == comboBox1) {
+            name = comboBox1.getSelectedItem().toString();
         }
-        if(e.getSource()==comboBox2){
-            String s1=comboBox2.getSelectedItem().toString();
-            if(s1.equals("加粗")){
-                style=Font.BOLD;
+        if (e.getSource() == comboBox2) {
+            String s1 = comboBox2.getSelectedItem().toString();
+            if (s1.equals("加粗")) {
+                style = Font.BOLD;
             }
-            if(s1.equals("倾斜")){
-                style=Font.ITALIC;
+            if (s1.equals("倾斜")) {
+                style = Font.ITALIC;
             }
-            if(s1.equals("常规")){
-                style=Font.PLAIN;
+            if (s1.equals("常规")) {
+                style = Font.PLAIN;
             }
         }
-        if(e.getSource()==comboBox3){
-            String s2=comboBox3.getSelectedItem().toString();
-            size=Integer.parseInt(s2);
+        if (e.getSource() == comboBox3) {
+            String s2 = comboBox3.getSelectedItem().toString();
+            size = Integer.parseInt(s2);
         }
 
     }
 
-    class MyActionListener3 implements ActionListener{
+    class MyActionListener3 implements ActionListener {
         public void actionPerformed(ActionEvent e2) {
-            Font f=new Font(name,style,size);
+            Font f = new Font(name, style, size);
             a1.setFont(f);
-            if(e2.getActionCommand()=="颜色"){
+            if (e2.getActionCommand() == "颜色") {
                 setcolor();
             }
         }
     }
-    void setcolor(){
-        Color   fontcolor=JColorChooser.showDialog(this,"字体颜色选择",a1.getForeground());
+
+    void setcolor() {
+        Color fontcolor = JColorChooser.showDialog(this, "字体颜色选择", a1.getForeground());
         a1.setForeground(fontcolor);
     }
 }
--- src/Wordwin.java
+++ src/Wordwin.java
@@ -7,121 +7,169 @@ import javax.swing.text.html.HTMLEditorKit;
 import java.awt.*;
 import java.io.*;
 import java.awt.event.*;
+
 public class Wordwin extends JFrame implements DocumentListener {
     /**
      *
      */
     private static final long serialVersionUID = 1L;
-    JMenuBar menubar=new JMenuBar();
-    JMenu file=new JMenu("文件");
-    JMenu edit=new JMenu("编辑");
-    JMenu geshi=new JMenu("格式");
-    JMenu look =new JMenu("查看");
-    JMenu help =new JMenu("帮助");
-    JTextArea wordArea=new JTextArea();
-    JTextPane wordPanel=new JTextPane();
+    JMenuBar menubar = new JMenuBar();
+    JMenu file = new JMenu("文件");
+    JMenu edit = new JMenu("编辑");
+    JMenu geshi = new JMenu("格式");
+    JTextArea wordArea = new JTextArea();
+    JTextPane wordPanel = new JTextPane();
     HTMLEditorKit htmlEditorKit = new HTMLEditorKit();
-    HTMLDocument text_html=(HTMLDocument)htmlEditorKit.createDefaultDocument();
+    HTMLDocument text_html = (HTMLDocument) htmlEditorKit.createDefaultDocument();
 
     JScrollPane imgScrollPane = new JScrollPane(wordArea);
-    JScrollPane imgScrollPane2=new JScrollPane(wordPanel);
-    String [] str1={"新建","打开","保存","另存为","页面设置","打印","退出"};
-    String [] str2={"剪切","复制","粘贴","查找","替换","insert"};
-    String [] str3={"自动换行","字体"};
-    Font f1=new Font("隶书",Font.PLAIN,15);
-    Search d1=new Search();
-    Font1 z1=new Font1();
-    Change c1=new Change();
-    int flag=0;
-    String source="";
-    public static void main(String[] args) {
-        JFrame wordwin=new Wordwin();
-        wordwin.setVisible(true);
+    JScrollPane imgScrollPane2 = new JScrollPane(wordPanel);
+    String[] str1 = {"新建", "打开", "保存", "另存为", "退出"};
+    String[] str2 = {"网络保存", "网络读取", "查找", "替换", "insert"};
+    String[] str3 = {"字体"};
+    Font f1 = new Font("隶书", Font.PLAIN, 15);
+    Search d1 = new Search();
+    Font1 z1 = new Font1();
+    Change c1 = new Change();
+    String source = "";
+    private boolean isNoChanged = true;
+    private String nowFilePath = "";
+    private boolean isSaved = false;
+    class MyActionListener1 implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() instanceof JMenuItem) {
+                if (e.getActionCommand().equals("新建")) {
+                    newfile();
+                }
+                if (e.getActionCommand().equals("打开")) {
+                    open();
+                }
+                if (e.getActionCommand().equals("保存")) {
+                    save();
+                }
+                if (e.getActionCommand().equals("另存为")) {
+                    saveAs();
+                }
+                if (e.getActionCommand().equals("退出")) {
+                    exit();
+                }
+                if (e.getActionCommand().equals("查找")) {
+                    d1.setVisible(true);
+                }
+                if (e.getActionCommand().equals("替换")) {
+                    c1.setVisible(true);
+                }
+                if (e.getActionCommand().equals("字体")) {
+                    z1.setVisible(true);
+                }
+                if (e.getActionCommand().equals("insert")) {
+                    insert();
+                    String txt = wordPanel.getText();
+                    wordArea.setText(txt);
+                }
+                if (e.getActionCommand().equals("网络保存")) {
+                    wordArea.cut();
+                }
+                if (e.getActionCommand().equals("网络读取")) {
+                    wordArea.copy();
+                }
+            }
+        }
     }
-    public Wordwin(){
+
+    Wordwin() {
         c1.set(wordArea);
         z1.set(wordArea);
         setTitle("文本编辑器");
         Toolkit kit = Toolkit.getDefaultToolkit();
         Dimension screenSize = kit.getScreenSize();//获取屏幕分辨率
-        setSize(screenSize.width/2,screenSize.height/2);//大小
-        setLocation(screenSize.width/4,screenSize.height/4);//位置
-        Container container=getContentPane();
-        container.setLayout(new GridLayout(2,1));
+        setSize(screenSize.width / 2, screenSize.height / 2);//大小
+        setLocation(screenSize.width / 4, screenSize.height / 4);//位置
+        Container container = getContentPane();
+        container.setLayout(new GridLayout(2, 1));
         add(imgScrollPane);
         add(imgScrollPane2);
         setJMenuBar(menubar);
         file.setFont(f1);
         edit.setFont(f1);
         geshi.setFont(f1);
-        look.setFont(f1);
-        help.setFont(f1);
         menubar.add(file);
         menubar.add(edit);
         menubar.add(geshi);
-        menubar.add(look);
-        menubar.add(help);
         wordPanel.setEditorKit(htmlEditorKit);
         wordPanel.setContentType("text/html");
         wordPanel.setDocument(text_html);
 
         wordArea.getDocument().addDocumentListener(this);
         wordPanel.getDocument().addDocumentListener(this);
-        for(int i=0;i<str1.length;i++){
-            JMenuItem item1= new JMenuItem(str1[i]);
+        for (int i = 0; i < str1.length; i++) {
+            JMenuItem item1 = new JMenuItem(str1[i]);
             item1.addActionListener(new MyActionListener1());
             item1.setFont(f1);
             file.add(item1);
         }
-        for(int i=0;i<str2.length;i++){
-            JMenuItem item2= new JMenuItem(str2[i]);
+        for (int i = 0; i < str2.length; i++) {
+            JMenuItem item2 = new JMenuItem(str2[i]);
             item2.addActionListener(new MyActionListener1());
             item2.setFont(f1);
             edit.add(item2);
         }
-        for(int i=0;i<str3.length;i++){
-            JMenuItem item3= new JMenuItem(str3[i]);
+        for (int i = 0; i < str3.length; i++) {
+            JMenuItem item3 = new JMenuItem(str3[i]);
             item3.addActionListener(new MyActionListener1());
             item3.setFont(f1);
             geshi.add(item3);
         }
-    }
-    public void changedUpdate(DocumentEvent e) {
-        flag=1;
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                exit();
+            }
+        });
     }
 
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        isNoChanged = false;
+    }
+
+    @Override
     public void insertUpdate(DocumentEvent e) {
-        flag=1;
+        isNoChanged = false;
     }
+
+    @Override
     public void removeUpdate(DocumentEvent e) {
-        flag=1;
+        isNoChanged = false;
     }
-    void open(){
-        FileDialog  filedialog=new FileDialog(this,"打开",FileDialog.LOAD);
+
+    private void open() {
+        FileDialog filedialog = new FileDialog(this, "打开", FileDialog.LOAD);
         filedialog.setVisible(true);
-        String path=filedialog.getDirectory();
-        String name=filedialog.getFile();
-        if(path!=null && name!=null){
+        String path = filedialog.getDirectory();
+        String name = filedialog.getFile();
+        if (path != null && name != null) {
             FileInputStream file = null;
             try {
-                file = new FileInputStream(path+name);
+                file = new FileInputStream(path + name);
             } catch (FileNotFoundException e) {
 
             }
-            InputStreamReader put =new InputStreamReader(file);
-            BufferedReader in=new BufferedReader(put);
-            String temp="";
+            InputStreamReader put = new InputStreamReader(file);
+            BufferedReader in = new BufferedReader(put);
+            String temp = "";
             String now = null;
             try {
-                now = (String)in.readLine();
+                now = in.readLine();
             } catch (IOException e) {
 
                 e.printStackTrace();
             }
-            while(now!=null){
-                temp+=now+"\r\n";
+            while (now != null) {
+                temp += now + "\r\n";
                 try {
-                    now=(String)in.readLine();
+                    now = in.readLine();
                 } catch (IOException e) {
 
                     e.printStackTrace();
@@ -133,118 +181,94 @@ public class Wordwin extends JFrame implements DocumentListener {
 
     }
 
-    void save(){
-        FileDialog  filedialog=new FileDialog(this,"保存",FileDialog.SAVE);
-        filedialog.setVisible(true);
-        if(filedialog.getDirectory()!=null && filedialog.getFile()!=null){
-            OutputStreamWriter out = null;
-            try {
-                out = new OutputStreamWriter(new FileOutputStream(filedialog.getDirectory()+filedialog.getFile()));
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-            try {
-                out.write(wordArea.getText());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            flag=0;
+    //LZN
+    private void save() {
+        FileDialog fileDialog = new FileDialog(this, "保存", FileDialog.SAVE);
+        fileDialog.setVisible(false);
+        if (!isSaved) {
+            fileDialog.setVisible(true);
+            nowFilePath = fileDialog.getDirectory() + fileDialog.getFile();
+            isSaved = true;
+        }
+        saveInner(nowFilePath);
+    }
+
+    //LZN
+    private void saveAs() {
+        FileDialog fileDialog = new FileDialog(this, "另存为", FileDialog.SAVE);
+        fileDialog.setVisible(true);
+        saveInner(fileDialog.getDirectory() + fileDialog.getFile());
+    }
+
+    //LZN
+    private void saveInner(String filePath) {
+        File file = new File(filePath);
+        BufferedWriter out = null;
+        try {
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+            out.write(wordArea.getText());
+            isNoChanged = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
             try {
                 out.close();
-                source=wordArea.getText();
+                source = wordArea.getText();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-    void newfile(){
-        if(flag==0){
+
+    private void newfile() {
+        if (isNoChanged) {
             wordArea.setText("");
-        }
-        if(flag==1){
-            int m=  JOptionPane.showConfirmDialog(this,"是否保存该文件");
-            if(m==0){
+        } else {
+            int m = JOptionPane.showConfirmDialog(this, "是否保存该文件");
+            if (m == 0) {
                 save();
                 wordArea.setText("");
             }
 
-            if(m==1){
+            if (m == 1) {
                 //System.exit(0);
                 wordArea.setText("");
-                flag=0;
+                isNoChanged = false;
             }
         }
     }
-    void exit(){
-        if(flag==0){
+
+    private void exit() {
+        if (isNoChanged) {
             System.exit(0);
-        }
-        if(flag==1){
-            int m=  JOptionPane.showConfirmDialog(this,"是否保存该文件");
-            if(m==0){
+        } else {
+            int m = JOptionPane.showConfirmDialog(this, "是否保存该文件");
+            if (m == 0) {
                 save();
             }
-            if(m==1){
+            if (m == 1) {
                 System.exit(0);
             }
         }
     }
 
-    void insert(){
+    private void insert() {
         try {
-            FileDialog  filedialog=new FileDialog(this,"打开",FileDialog.LOAD);
+            FileDialog filedialog = new FileDialog(this, "打开", FileDialog.LOAD);
             filedialog.setVisible(true);
-            String path=filedialog.getDirectory();
-            String url=filedialog.getFile();
-            String html="<img src=\"file:///"+path+url+"\">";
+            String path = filedialog.getDirectory();
+            String url = filedialog.getFile();
+            String html = "<img src=\"file:///" + path + url + "\">";
             System.out.println(html);
             htmlEditorKit.insertHTML(text_html, wordPanel.getCaretPosition(), html, 0, 0, HTML.Tag.IMG);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
-    class MyActionListener1 implements ActionListener{
-        public void actionPerformed(ActionEvent e) {
-            if(e.getSource()instanceof JMenuItem){
-                if(e.getActionCommand()=="剪切"){
-                    wordArea.cut();
-                }
-                if(e.getActionCommand()=="复制"){
-                    wordArea.copy();
-                }
-                if(e.getActionCommand()=="粘贴"){
-                    wordArea.paste();
-                }
-                if(e.getActionCommand()=="查找"){
-                    d1.setVisible(true);
-                }
-                if(e.getActionCommand()=="字体"){
-                    z1.setVisible(true);
-                }
-                if(e.getActionCommand()=="替换"){
-                    c1.setVisible(true);
-                }
-                if(e.getActionCommand()=="打开"){
-                    open();
-                }
-                if(e.getActionCommand()=="保存"){
-                    save();
-                }
-                if(e.getActionCommand()=="新建"){
-                    newfile();
-                }
-                if(e.getActionCommand()=="insert"){
-                    insert();
-                    String txt=wordPanel.getText();
-                    wordArea.setText(txt);
-                }
-                if(e.getActionCommand()=="退出"){
-                    exit();
-                }
-            }
 
-        }
-
+    public static void main(String[] args) {
+        JFrame wordwin = new Wordwin();
+        wordwin.setVisible(true);
     }
 }
